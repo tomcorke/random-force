5a0791ea05a4fd358f48c82c63aef00e8d1c478b
Tom Corke <tom.corke@skyscanner.net>
2025-09-15 16:38:37 +0100

    non-windows paths

--- src/scripts/build-metadata.ts
+++ src/scripts/build-metadata.ts
@@ -1,9 +1,9 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
-import simpleGit from 'simple-git';
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import simpleGit from "simple-git";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
 // get current commit sha and datetime
 // and put them in a json file in /src/sections/data/build-metadata.json
@@ -11,19 +11,18 @@ import { fileURLToPath } from 'url';
 export async function getGitMetadata() {
   const git = simpleGit();
   try {
-    const log = await git.log(['-1']);
-    const commitSha = log.latest?.hash || 'unknown';
+    const log = await git.log(["-1"]);
+    const commitSha = log.latest?.hash || "unknown";
     const commitDate = log.latest?.date || new Date().toISOString();
 
     return {
       commitSha,
       commitDate,
     };
-  }
-  catch (error) {
-    console.error('Error fetching git metadata:', error);
+  } catch (error) {
+    console.error("Error fetching git metadata:", error);
     return {
-      commitSha: 'unknown',
+      commitSha: "unknown",
       commitDate: new Date().toISOString(),
     };
   }
@@ -34,11 +33,11 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 export async function saveGitMetadata() {
   const metadata = await getGitMetadata();
 
-  const metadataPath = path.join(__dirname, '..\\data', 'build-metadata.json');
+  const metadataPath = path.join(__dirname, "../data", "build-metadata.json");
   fs.writeFileSync(metadataPath, JSON.stringify(metadata, null, 2));
 }
 
 // Call this function to save the metadata when the app starts
 saveGitMetadata().catch((error) => {
-  console.error('Failed to save git metadata:', error);
+  console.error("Failed to save git metadata:", error);
 });
