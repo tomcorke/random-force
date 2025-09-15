35dc6a701b698a874c60e0f3c505a0b644a3f783
Tom Corke <tom@tomcorke.com>
2025-09-15 12:04:43 +0100

    nox

--- src/data/generated/operators/Nox/index.ts
+++ src/data/generated/operators/Nox/index.ts
@@ -2,7 +2,7 @@ const data = {
   "src": "./images/nox.png",
   "icon": "https://deltaforcewiki.vasdgame.com/playerhub/40001/operator/abilities/10033.png",
   "name": "Nox",
-  "role": "scout"
+  "role": "assault"
 };
 
 import image from './image.png';
